a71ec75e652b4596e9a65a7c2ee6db89bbe1d929
stanypub <stanypub@75727209-5053-0410-b34a-2f5311fc09a8>
2010-11-10 23:21:39 +0000

    arch patched

---
+++ arch.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [ ! -f arch.sh ] || [ ! -f img2epub.py ]; then
+    echo 1>&2 "arch.sh must be launch from the install directory"
+    exit 1
+fi
+
+VERSION=$(python img2epub.py --version | head -n 1 | cut -d ' ' -f 2)
+mkdir -p ../img2epub-$VERSION
+cp -rf *  ../img2epub-$VERSION
+find ../img2epub-$VERSION -iname '*.pyc' -exec rm \{\} \;
+cd ..
+tar --exclude=.svn -czf img2epub-$VERSION.tgz img2epub-$VERSION
+md5sum img2epub-$VERSION.tgz > img2epub-$VERSION.md5
+rm -rf img2epub-$VERSION
